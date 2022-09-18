17d178792046cb7b6e1bbb63fe6359064487adf1
Raymond Lam <raymond.m.lam@gmail.com>
2022-09-18 17:53:33 +0200

    setting state for formatted address

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import SearchForm from "../components/SearchForm";
 import useCurrentLocation from "../hooks/useCurrentLocation";
@@ -5,17 +6,19 @@ import { getFormattedAddress } from "../services/GoogleAPI";
 
 const HomePage = () => {
 	const { getCurrentLocation, currentAddress } = useCurrentLocation();
+	const [formattedAddress, setFormattedAddress] = useState("");
 
 	console.log("Current address on location", currentAddress);
 
 	const handleSearch = async (address) => {
 		const addressResponse = await getFormattedAddress(address);
 
-		console.log(
-			"Searched for this address:",
-			addressResponse.results[0].formatted_address
-		);
+		setFormattedAddress(addressResponse.results[0].formatted_address);
 	};
+
+	useEffect(() => {
+		console.log("Searched for this address:", formattedAddress);
+	}, [formattedAddress]);
 
 	return (
 		<div className="flex">
