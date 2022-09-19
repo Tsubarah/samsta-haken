1c4e7e1c82cb732b3ffe3699ee6a6d257c2167a9
Raymond Lam <raymond.m.lam@gmail.com>
2022-09-19 15:15:45 +0200

    commented out devtools for react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import { ReactQueryDevtools } from 'react-query/devtools'
-import HomePage from './pages/HomePage'
-import '../src/assets/scss/App.scss'
+import { Routes, Route } from "react-router-dom";
+import { ReactQueryDevtools } from "react-query/devtools";
+import HomePage from "./pages/HomePage";
+import "../src/assets/scss/App.scss";
 
 function App() {
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+			</Routes>
 
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={ <HomePage /> }/>
-      </Routes>
-
-      <ReactQueryDevtools position='bottom-right' />
-    </div>
-  )
+			{/* <ReactQueryDevtools position='bottom-right' /> */}
+		</div>
+	);
 }
 
-export default App
+export default App;
