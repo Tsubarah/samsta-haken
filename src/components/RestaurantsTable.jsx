0e2eeab61a4de612d65167be633c867b9f0be8ab
Raymond Lam <raymond.m.lam@gmail.com>
2022-09-22 14:27:42 +0200

    hardcoded headers

--- src/components/RestaurantsTable.jsx
+++ src/components/RestaurantsTable.jsx
@@ -4,7 +4,7 @@ const RestaurantsTable = ({ restaurants }) => {
 	const navigate = useNavigate();
 
 	return (
-		<div className="overflow-x-auto scrollbar-thin scrollbar-thumb-gray-100 scrollbar-track-black">
+		<div className="overflow-x-auto overflow-y-auto scrollbar-thin scrollbar-thumb-gray-100 scrollbar-track-black">
 			<table className="table table-zebra w-full">
 				{/* <!-- head --> */}
 				<thead>
@@ -16,49 +16,47 @@ const RestaurantsTable = ({ restaurants }) => {
 						<th>Typ av kök</th>
 						<th>Typ av matställe</th>
 						<th>Utbud</th>
-						{restaurants &&
-							restaurants.map((restaurant) =>
-								restaurant.socials.map((social, i) => (
-									<th key={i}>{social.title}</th>
-								))
-							)}
+						<th>Hemsida</th>
+						<th>E-post</th>
+						<th>Tel</th>
+						<th>Facebook</th>
+						<th>Instagram</th>
 						<th>Redigera</th>
 						<th>Godkänd</th>
 					</tr>
 				</thead>
 				<tbody>
-					{restaurants &&
-						restaurants.map((restaurant, i) => (
-							<tr key={restaurant.id}>
-								<td>{i + 1}</td>
-								<td>{restaurant.name}</td>
-								<td>{restaurant.address}</td>
-								<td>{restaurant.city}</td>
-								<td>{restaurant.cuisine}</td>
-								<td>{restaurant.type_of_place}</td>
-								<td>{restaurant.offers_food}</td>
+					{restaurants.map((restaurant, i) => (
+						<tr key={restaurant.id}>
+							<td>{i + 1}</td>
+							<td>{restaurant.name}</td>
+							<td>{restaurant.address}</td>
+							<td>{restaurant.city}</td>
+							<td>{restaurant.cuisine}</td>
+							<td>{restaurant.type_of_place}</td>
+							<td>{restaurant.offers_food}</td>
 
-								{restaurant.socials.map((social, i) => (
-									<td key={i}>{social.value}</td>
-								))}
-								<td>
-									<button
-										className="btn btn-sm btn-info"
-										onClick={() => navigate(`restaurants/${restaurant.id}`)}
-									>
-										Redigera
-									</button>
-								</td>
+							{restaurant.socials.map((social, i) => (
+								<td key={i}>{social.value}</td>
+							))}
+							<td>
+								<button
+									className="btn btn-sm btn-info"
+									onClick={() => navigate(`restaurants/${restaurant.id}`)}
+								>
+									Redigera
+								</button>
+							</td>
 
-								<td className="text-center">
-									{restaurant.accepted ? (
-										<button className="btn btn-success btn-sm">J</button>
-									) : (
-										<button className="btn btn-error btn-sm">N</button>
-									)}
-								</td>
-							</tr>
-						))}
+							<td className="text-center">
+								{restaurant.accepted ? (
+									<button className="btn btn-success btn-sm">J</button>
+								) : (
+									<button className="btn btn-error btn-sm">N</button>
+								)}
+							</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
 		</div>
