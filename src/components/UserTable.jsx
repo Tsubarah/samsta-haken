d22847d79b6d6e544e1bde49747564fccb6ad165
Raymond Lam <raymond.m.lam@gmail.com>
2022-09-22 15:43:18 +0200

    fixed scroll style and created content for restaurant page

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,56 +1,58 @@
 import { useAuthContext } from "../contexts/AuthContext";
 
 const UserTable = ({ users }) => {
-  console.log(users);
+	console.log(users);
 
-  const { updateAdmin } = useAuthContext();
+	const { updateAdmin } = useAuthContext();
 
-  const handleUpdateAdmin = (e) => {
-    updateAdmin(e.target.id);
-  };
+	const handleUpdateAdmin = (e) => {
+		updateAdmin(e.target.id);
+	};
 
-  return (
-    <div className="overflow-x-auto">
-      <table className="table w-full">
-        {/* <!-- head --> */}
-        <thead>
-          <tr>
-            <th></th>
-            <th>Image</th>
-            <th>Email</th>
-            <th>Admin</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users &&
-            users.map((user) => (
-              <tr key={user.id}>
-                <th>1</th>
-                <td>Null</td>
-                <td>{user.email}</td>
-                <td>
-                  {user.admin ? (
-                    <button
-                      id={user.id}
-                      onClick={handleUpdateAdmin}
-                      className="btn btn-success">
-                      J
-                    </button>
-                  ) : (
-                    <button
-                      id={user.id}
-                      onClick={handleUpdateAdmin}
-                      className="btn btn-error">
-                      N
-                    </button>
-                  )}
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div className="overflow-x-auto overflow-y-auto scrollbar-thin scrollbar-thumb-gray-100 scrollbar-track-black">
+			<table className="table table-zebra w-full">
+				{/* <!-- head --> */}
+				<thead>
+					<tr>
+						<th></th>
+						<th>Image</th>
+						<th>Email</th>
+						<th>Admin</th>
+					</tr>
+				</thead>
+				<tbody>
+					{users &&
+						users.map((user) => (
+							<tr key={user.id}>
+								<th>1</th>
+								<td>Null</td>
+								<td>{user.email}</td>
+								<td>
+									{user.admin ? (
+										<button
+											id={user.id}
+											onClick={handleUpdateAdmin}
+											className="btn btn-success"
+										>
+											J
+										</button>
+									) : (
+										<button
+											id={user.id}
+											onClick={handleUpdateAdmin}
+											className="btn btn-error"
+										>
+											N
+										</button>
+									)}
+								</td>
+							</tr>
+						))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default UserTable;
