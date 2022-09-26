239b745e315ccd6a090fbcada649b7aaff422ec6
Raymond Lam <raymond.m.lam@gmail.com>
2022-09-26 17:52:33 +0200

    trying to test login component

---
+++ src/components/___test___/Login.test.jsx
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+import {
+	screen,
+	renderWithContextProvider,
+	render,
+} from "../../utils/test-utils";
+import Login from "../Login";
+
+describe("Test login input fields", () => {
+	it("render input fields", () => {
+		// render
+		// renderWithContextProvider(<Login />);
+		// render(<Login />);
+		// find
+		// const emailFieldEl = screen.getByRole("textbox", {
+		// 	name: /^email/i,
+		// });
+		// const passwordFieldEl = screen.getAllByPlaceholderText(/^password/i);
+		// assert
+		// expect(emailFieldEl).toBeInTheDocument();
+		// expect(passwordFieldEl).toBeInTheDocument();
+	});
+});
--- src/utils/test-utils.jsx
+++ src/utils/test-utils.jsx
@@ -1,8 +1,13 @@
 import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "../contexts/AuthContext";
 
 export const renderWithBrowserRouter = (component) => {
 	return render(<BrowserRouter>{component}</BrowserRouter>);
+};
+
+export const renderWithContextProvider = (component) => {
+	return render(<AuthContextProvider>{component}</AuthContextProvider>);
 };
 
 export * from "@testing-library/react";
