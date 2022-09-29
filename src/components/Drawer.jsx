b94a6e9f24040381d7270df17d00d27764ff1239
Hanna Håkanson <hannahakanson@live.se>
2022-09-29 19:05:36 +0200

    First draft of restaurant card

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,31 +1,59 @@
-import { useEffect } from "react"
+import { useState } from "react"
 import { useAuthContext } from "../contexts/AuthContext"
+import useGetCollection from "../hooks/useGetCollection"
+import useGetDocument from "../hooks/useGetDocument"
 import useGetRestaurants from "../hooks/useGetRestaurants"
 import RestaurantCard from "./RestaurantCard"
+import { MdPlace } from "react-icons/md";
 
 const Drawer = ({ children }) => {
   const restaurantQuery = useGetRestaurants()
   const { drawerIsOpen, setDrawerIsOpen } = useAuthContext()
+  const [showRestaurantCard, setShowRestaurantCard] = useState(false)
+  const [restaurantData, setRestaurantData] = useState(null)
+  let restaurant;
+
+  const handleClick = async (e) => {
+    restaurant = restaurantQuery.data?.find(restaurant => restaurant.id === e.currentTarget.id)
+    setRestaurantData(restaurant)
+    setShowRestaurantCard(!showRestaurantCard)
+  }
 
   return (
-    <div className="drawer h-full drawer-end">
-      <input id="my-drawer-4" type="checkbox" checked={drawerIsOpen} onChange={setDrawerIsOpen} className="drawer-toggle" />
+    <div className="drawer h-full drawer-end relative">
+      <input
+        id="my-drawer-4"
+        type="checkbox"
+        checked={drawerIsOpen}
+        onChange={setDrawerIsOpen}
+        className="drawer-toggle"
+      />
       <div className="drawer-content">
         {children}
-        <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary hidden">Open drawer</label>
-      </div> 
+        <label
+          htmlFor="my-drawer-4"
+          className="drawer-button btn btn-primary hidden">
+          Open drawer
+        </label>
+      </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
         <ul className="menu overflow-y-auto w-96 bg-base-100 text-base-content">
-          {restaurantQuery.data?.map(restaurant => (
-            <li key={restaurant.id}>
-              <RestaurantCard data={restaurant} />
+          {restaurantQuery.data?.map((restaurant) => (
+            <li id={restaurant.id} key={restaurant.id} onClick={handleClick}>
+              <div className="flex justify-between p-6">
+                {restaurant.name}
+                <p className="text-xs opacity-50">{restaurant.city}</p>
+              </div>
             </li>
           ))}
-        </ul>  
+        </ul>
+      </div>
+      <div className="overflow-y-auto bg-primary flex absolute lg:right-96 lg:top-auto z-50">
+        {showRestaurantCard && <RestaurantCard restaurant={restaurantData} />}
       </div>
     </div>
-  )
+  );
 }
 
 export default Drawer
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,36 +1,125 @@
+import { BiReplyAll } from "react-icons/bi";
+import { MdOutlineCancel } from "react-icons/md";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiOutlineGlobal } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiFillPhone } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
 
-const RestaurantCard = ({ data }) => {
+
+const RestaurantCard = ({ restaurant }) => {
+  const [showRestaurantCard, setShowRestaurantCard] = useState(false)
+  console.log(restaurant);
+  let icon;
 
   return (
-    <>
-      <div className="card max-h-50 card-side bg-base-100 shadow-xl">
-        <div className="flex flex-col">
-          <div className="card-body grid grid-cols-12 items-between px-0 pb-0">
-
-            <img src="https://placeimg.com/200/280/arch" alt="restaurant" className="col-span-5 object-cover"/>
-
-            <div className="item-description col-start-7 col-span-6">
-                <h2 className="card-title">{data.name}</h2>
-                <p>{data.cuisine}</p>
-                <p>{data.city}</p>
-                <p>{data.offers_food}</p>
-                <p>{data.address}</p>
-            </div>
-            <div className="collapse col-span-12 justify-start">
-              <input type="checkbox" /> 
-              <div className="collapse-title p-0 text-xl font-medium">
-                🔽
-              </div>
-              <div className="collapse-content"> 
-                <p>{data.description}</p>
-              </div>
-            </div>
-
-            </div>
-        </div>
+    <div className="card card-compact restaurant-card grow rounded-none lg:w-96 w-full h-full bg-base-100 shadow-xl overflow-y-auto scrollbar-hide">
+      <div className="col-span-full lg:hidden grid grid-rows-2">
+        <MdOutlineCancel
+          size={15}
+          className="cursor-pointer justify-self-end text-primary hover:text-error"
+        />
       </div>
-    </>
-  )
-}
+      <figure>
+        <img src="https://placeimg.com/400/225/arch" alt="Restaurant" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{restaurant.name}</h2>
+        <div className="divider"></div>
+        <div className="flex">
+          <div className="badge">{restaurant.type_of_place}</div>
+          <div className="badge">{restaurant.offers_food}</div>
+          <div className="badge">{restaurant.cuisine}</div>
+        </div>
+        <div className="pt-4">
+          <h3 className="font-bold">Recension</h3>
+          <p>{restaurant.description}</p>
+        </div>
 
-export default RestaurantCard
+        <div className="divider"></div>
+
+        <ul>
+          {restaurant.socials.map((element) => {
+            console.log("ELment: ", element.value);
+            if (element.value != "") {
+              icon = element.title;
+              switch (icon) {
+                case "hemsida":
+                  return (
+                    <li className="p-2">
+                      <p className="text-xs inline-flex">
+                        <AiOutlineGlobal
+                          size={20}
+                          className="mr-2 cursor-pointer text-primary hover:text-error"
+                        />{" "}
+                        {element.value}
+                      </p>
+                    </li>
+                  );
+                  break;
+                case "e-post":
+                  return (
+                    <li className="p-2">
+                      <p className="text-xs inline-flex">
+                        <AiOutlineMail
+                          size={20}
+                          className="mr-2 cursor-pointer text-primary hover:text-error"
+                        />
+                        {element.value}
+                      </p>
+                    </li>
+                  );
+                  break;
+                case "tel":
+                  return (
+                    <li className="p-2">
+                      <p className="text-xs inline-flex">
+                        <AiFillPhone
+                          size={20}
+                          className="mr-2 cursor-pointer text-primary hover:text-error"
+                        />{" "}
+                        {element.value}
+                      </p>
+                    </li>
+                  );
+                  break;
+                case "facebook":
+                  return (
+                    <li className="p-2">
+                      <p className="text-xs inline-flex">
+                        <AiFillFacebook
+                          size={20}
+                          className="mr-2 cursor-pointer text-primary hover:text-error"
+                        />
+                        {element.value}
+                      </p>
+                    </li>
+                  );
+                  break;
+                case "instagram":
+                  return (
+                    <li className="p-2">
+                      <p className="text-xs inline-flex">
+                        <AiFillInstagram
+                          size={20}
+                          className="mr-2 cursor-pointer text-primary hover:text-error"
+                        />
+                        {element.value}
+                      </p>
+                    </li>
+                  );
+                  break;
+                default:
+                  <li className="p-2">No socials available</li>;
+              }
+            }
+          })}
+        </ul>
+
+        <div className="card-actions justify-end"></div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCard;
