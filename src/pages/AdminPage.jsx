431e9c05c00fd10df3fd442cf60a06c4bdc2391e
Raymond Lam <raymond.m.lam@gmail.com>
2022-09-30 22:07:57 +0200

    created table of new uploaded photos so admin can confirm them. Looping thru all restaurants and finding photos array and then pushing them to new array

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -3,17 +3,12 @@ import useGetCollection from "../hooks/useGetCollection";
 import { useAuthContext } from "../contexts/AuthContext";
 import UserTable from "../components/UserTable";
 import RestaurantsTable from "../components/RestaurantsTable";
+import ImagesTable from "../components/ImagesTable";
 
 const AdminPage = () => {
 	const [showUsersTable, setShowUsersTable] = useState(false);
 	const [showRestaurantsTable, setShowRestaurantsTable] = useState(false);
-
-	const handleShowUsersTable = () => {
-		setShowUsersTable(!showUsersTable);
-	};
-	const handleShowRestaurantsTable = () => {
-		setShowRestaurantsTable(!showRestaurantsTable);
-	};
+	const [showImagesTable, setShowImagesTable] = useState(false);
 
 	const { currentUser } = useAuthContext();
 
@@ -21,35 +16,47 @@ const AdminPage = () => {
 	const { data: restaurants } = useGetCollection("restaurants");
 
 	return (
-    <div className="h-full flex flex-col overflow-hidden">
-      <div className="flex justify-center gap-4 py-4">
-        <h1 className="text-xl">
-          Välkommen till admin-sidan: {""}
-          <span className="font-semibold">
-            {currentUser.displayName
-              ? currentUser.displayName
-              : currentUser.email}
-          </span>
-        </h1>
-      </div>
+		<div className="h-full flex flex-col overflow-hidden">
+			<div className="flex justify-center gap-4 py-4">
+				<h1 className="text-xl">
+					Välkommen till admin-sidan: {""}
+					<span className="font-semibold">
+						{currentUser.displayName
+							? currentUser.displayName
+							: currentUser.email}
+					</span>
+				</h1>
+			</div>
 
-      <button
-        className="btn btn-block btn-primary btn-sm"
-        onClick={handleShowUsersTable}>
-        Användare
-      </button>
-      {showUsersTable && <UserTable users={users} />}
+			<button
+				className="btn btn-block btn-primary btn-sm"
+				onClick={() => setShowUsersTable(!showUsersTable)}
+			>
+				Användare
+			</button>
+			{showUsersTable && <UserTable users={users} />}
 
-      <div className="divider"></div>
+			<div className="divider"></div>
 
-      <button
-        className="btn btn-block btn-primary btn-sm"
-        onClick={handleShowRestaurantsTable}>
-        Restauranger
-      </button>
-      {showRestaurantsTable && <RestaurantsTable restaurants={restaurants} />}
-    </div>
-  );
+			<button
+				className="btn btn-block btn-primary btn-sm"
+				onClick={() => setShowRestaurantsTable(!showRestaurantsTable)}
+			>
+				Restauranger
+			</button>
+			{showRestaurantsTable && <RestaurantsTable restaurants={restaurants} />}
+
+			<div className="divider"></div>
+
+			<button
+				className="btn btn-block btn-primary btn-sm"
+				onClick={() => setShowImagesTable(!showImagesTable)}
+			>
+				Inskickade bilder
+			</button>
+			{showImagesTable && <ImagesTable restaurants={restaurants} />}
+		</div>
+	);
 };
 
 export default AdminPage;
