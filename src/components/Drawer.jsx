04ddd301781ba1e421f2f3263827b7b59221c0f1
Raymond Lam <raymond.m.lam@gmail.com>
2022-09-30 13:42:53 +0200

    styling drawer

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,59 +1,70 @@
-import { useState } from "react"
-import { useAuthContext } from "../contexts/AuthContext"
-import useGetCollection from "../hooks/useGetCollection"
-import useGetDocument from "../hooks/useGetDocument"
-import useGetRestaurants from "../hooks/useGetRestaurants"
-import RestaurantCard from "./RestaurantCard"
+import { useState } from "react";
+import { useAuthContext } from "../contexts/AuthContext";
+import useGetCollection from "../hooks/useGetCollection";
+import useGetDocument from "../hooks/useGetDocument";
+import useGetRestaurants from "../hooks/useGetRestaurants";
+import RestaurantCard from "./RestaurantCard";
 import { MdPlace } from "react-icons/md";
 
 const Drawer = ({ children }) => {
-  const restaurantQuery = useGetRestaurants()
-  const { drawerIsOpen, setDrawerIsOpen } = useAuthContext()
-  const [showRestaurantCard, setShowRestaurantCard] = useState(false)
-  const [restaurantData, setRestaurantData] = useState(null)
-  let restaurant;
+	const restaurantQuery = useGetRestaurants();
+	const { drawerIsOpen, setDrawerIsOpen } = useAuthContext();
+	const [showRestaurantCard, setShowRestaurantCard] = useState(false);
+	const [restaurantData, setRestaurantData] = useState(null);
+	let restaurant;
 
-  const handleClick = async (e) => {
-    restaurant = restaurantQuery.data?.find(restaurant => restaurant.id === e.currentTarget.id)
-    setRestaurantData(restaurant)
-    setShowRestaurantCard(!showRestaurantCard)
-  }
+	const handleClick = async (e) => {
+		restaurant = restaurantQuery.data?.find(
+			(restaurant) => restaurant.id === e.currentTarget.id
+		);
+		setRestaurantData(restaurant);
+		setShowRestaurantCard(!showRestaurantCard);
+	};
 
-  return (
-    <div className="drawer h-full drawer-end relative">
-      <input
-        id="my-drawer-4"
-        type="checkbox"
-        checked={drawerIsOpen}
-        onChange={setDrawerIsOpen}
-        className="drawer-toggle"
-      />
-      <div className="drawer-content">
-        {children}
-        <label
-          htmlFor="my-drawer-4"
-          className="drawer-button btn btn-primary hidden">
-          Open drawer
-        </label>
-      </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
-        <ul className="menu overflow-y-auto w-96 bg-base-100 text-base-content">
-          {restaurantQuery.data?.map((restaurant) => (
-            <li id={restaurant.id} key={restaurant.id} onClick={handleClick}>
-              <div className="flex justify-between p-6">
-                {restaurant.name}
-                <p className="text-xs opacity-50">{restaurant.city}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="overflow-y-auto bg-primary flex absolute lg:right-96 lg:top-auto z-50">
-        {showRestaurantCard && <RestaurantCard restaurant={restaurantData} />}
-      </div>
-    </div>
-  );
-}
+	return (
+		<div className="drawer h-full drawer-end relative">
+			<input
+				id="my-drawer-4"
+				type="checkbox"
+				checked={drawerIsOpen}
+				onChange={setDrawerIsOpen}
+				className="drawer-toggle"
+			/>
+			<div className="drawer-content">
+				{children}
+				<label
+					htmlFor="my-drawer-4"
+					className="drawer-button btn btn-primary hidden"
+				>
+					Open drawer
+				</label>
+			</div>
+			<div className="drawer-side grid-cols-1 lg:grid-cols-3 scrollbar-hide">
+				<label htmlFor="my-drawer-4" className="drawer-overlay hidden"></label>
 
-export default Drawer
+				<div className="flex flex-wrap relative bg-base-100 lg:col-span-full lg:col-end-4 scrollbar-thin scrollbar-thumb-base-content scrollbar-track-black">
+					{showRestaurantCard && drawerIsOpen && (
+						<RestaurantCard restaurant={restaurantData} />
+					)}
+
+					<div className="px-2 font-semibold text-white text-lg lg:hidden bg-base-content w-full">
+						<div className="divider">Restauranger</div>
+					</div>
+
+					<ul className="menu w-full lg:w-96 text-base-content">
+						{restaurantQuery.data?.map((restaurant) => (
+							<li id={restaurant.id} key={restaurant.id} onClick={handleClick}>
+								<div className="flex justify-between p-6">
+									{restaurant.name}
+									<p className="text-xs opacity-50">{restaurant.city}</p>
+								</div>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Drawer;
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,125 +1,182 @@
-import { BiReplyAll } from "react-icons/bi";
-import { MdOutlineCancel } from "react-icons/md";
-import { AiFillFacebook } from "react-icons/ai";
-import { AiOutlineGlobal } from "react-icons/ai";
-import { AiOutlineMail } from "react-icons/ai";
-import { AiFillPhone } from "react-icons/ai";
-import { AiFillInstagram } from "react-icons/ai";
+import { MdOutlineCancel, MdAddPhotoAlternate } from "react-icons/md";
+import {
+	AiFillFacebook,
+	AiOutlineGlobal,
+	AiOutlineMail,
+	AiFillPhone,
+	AiFillInstagram,
+	AiOutlineCloudUpload,
+} from "react-icons/ai";
 
+import { useAuthContext } from "../contexts/AuthContext";
+import { useState } from "react";
 
 const RestaurantCard = ({ restaurant }) => {
-  //const [showRestaurantCard, setShowRestaurantCard] = useState(false)
-  console.log(restaurant);
-  let icon;
+	// const [showRestaurantCard, setShowRestaurantCard] = useState(false);
+	// console.log(restaurant);
+	let icon;
+	const { currentUser } = useAuthContext();
+	const [image, setImage] = useState(null);
 
-  return (
-    <div className="card card-compact restaurant-card grow rounded-none lg:w-96 w-full h-full bg-base-100 shadow-xl overflow-y-auto scrollbar-hide">
-      <div className="col-span-full lg:hidden grid grid-rows-2">
-        <MdOutlineCancel
-          size={15}
-          className="cursor-pointer justify-self-end text-primary hover:text-error"
-        />
-      </div>
-      <figure>
-        <img src="https://placeimg.com/400/225/arch" alt="Restaurant" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{restaurant.name}</h2>
-        <div className="divider"></div>
-        <div className="flex">
-          <div className="badge">{restaurant.type_of_place}</div>
-          <div className="badge">{restaurant.offers_food}</div>
-          <div className="badge">{restaurant.cuisine}</div>
-        </div>
-        <div className="pt-4">
-          <h3 className="font-bold">Recension</h3>
-          <p>{restaurant.description}</p>
-        </div>
+	console.log("current user", currentUser);
 
-        <div className="divider"></div>
+	const handleFileChange = (e) => {
+		if (!e.target.files.length) {
+			setImage(null);
+			return;
+		}
 
-        <ul>
-          {restaurant.socials.map((element) => {
-            console.log("ELment: ", element.value);
-            if (element.value != "") {
-              icon = element.title;
-              switch (icon) {
-                case "hemsida":
-                  return (
-                    <li className="p-2">
-                      <p className="text-xs inline-flex">
-                        <AiOutlineGlobal
-                          size={20}
-                          className="mr-2 cursor-pointer text-primary hover:text-error"
-                        />{" "}
-                        {element.value}
-                      </p>
-                    </li>
-                  );
-                  break;
-                case "e-post":
-                  return (
-                    <li className="p-2">
-                      <p className="text-xs inline-flex">
-                        <AiOutlineMail
-                          size={20}
-                          className="mr-2 cursor-pointer text-primary hover:text-error"
-                        />
-                        {element.value}
-                      </p>
-                    </li>
-                  );
-                  break;
-                case "tel":
-                  return (
-                    <li className="p-2">
-                      <p className="text-xs inline-flex">
-                        <AiFillPhone
-                          size={20}
-                          className="mr-2 cursor-pointer text-primary hover:text-error"
-                        />{" "}
-                        {element.value}
-                      </p>
-                    </li>
-                  );
-                  break;
-                case "facebook":
-                  return (
-                    <li className="p-2">
-                      <p className="text-xs inline-flex">
-                        <AiFillFacebook
-                          size={20}
-                          className="mr-2 cursor-pointer text-primary hover:text-error"
-                        />
-                        {element.value}
-                      </p>
-                    </li>
-                  );
-                  break;
-                case "instagram":
-                  return (
-                    <li className="p-2">
-                      <p className="text-xs inline-flex">
-                        <AiFillInstagram
-                          size={20}
-                          className="mr-2 cursor-pointer text-primary hover:text-error"
-                        />
-                        {element.value}
-                      </p>
-                    </li>
-                  );
-                  break;
-                default:
-                  <li className="p-2">No socials available</li>;
-              }
-            }
-          })}
-        </ul>
+		setImage(e.target.files[0]);
+		console.log("File changed!", e.target.files[0]);
+	};
 
-        <div className="card-actions justify-end"></div>
-      </div>
-    </div>
-  );
+	const handleUpload = () => {
+		if (!currentUser) {
+			return alert("Du måste vara inloggad för att skicka in bild");
+		}
+	};
+
+	return (
+		<div className="card card-compact rounded-none lg:w-96 bg-base-100 shadow-xl scrollbar-thin scrollbar-thumb-base-content scrollbar-track-black">
+			{/* <div className="col-span-full lg:hidden grid grid-rows-2 p-2">
+				<MdOutlineCancel
+					size={20}
+					className="cursor-pointer justify-self-end text-base-content hover:text-error"
+				/>
+			</div> */}
+
+			<figure>
+				<img src="https://placeimg.com/400/225/arch" alt="Restaurant" />
+			</figure>
+
+			<div className="card-body">
+				<h2 className="card-title">{restaurant.name}</h2>
+
+				<div className="upload-photo flex items-center justify-between">
+					<label
+						htmlFor="image"
+						className="border border-base-content p-3 rounded-full self-start flex items-center gap-2"
+					>
+						<MdAddPhotoAlternate size={25} />
+
+						{image ? (
+							<span className="text-gray-400 text-sm italic">
+								{image.name} {Math.round(image.size / 1024)} kB
+							</span>
+						) : (
+							<span className="text-gray-400 text-sm italic">
+								Ingen vald bild
+							</span>
+						)}
+
+						<input
+							type="file"
+							id="image"
+							className="mt-2 hidden"
+							onChange={handleFileChange}
+						/>
+					</label>
+
+					{image && (
+						<button className="btn btn-circle">
+							<AiOutlineCloudUpload size={25} />
+						</button>
+					)}
+				</div>
+				<div className="divider"></div>
+
+				<div className="flex">
+					<div className="badge">{restaurant.type_of_place}</div>
+					<div className="badge">{restaurant.offers_food}</div>
+					<div className="badge">{restaurant.cuisine}</div>
+				</div>
+
+				<div className="pt-4">
+					<h3 className="font-bold">Recension</h3>
+					<p>{restaurant.description}</p>
+				</div>
+
+				<div className="divider"></div>
+
+				<ul>
+					{restaurant.socials.map((element, i) => {
+						// console.log("ELment: ", element.value);
+						if (element.value != "") {
+							icon = element.title;
+							switch (icon) {
+								case "hemsida":
+									return (
+										<li key={i} className="p-2">
+											<p className="text-xs inline-flex items-center">
+												<AiOutlineGlobal
+													size={20}
+													className="mr-2 cursor-pointer text-primary hover:text-error"
+												/>{" "}
+												{element.value}
+											</p>
+										</li>
+									);
+								case "e-post":
+									return (
+										<li key={i} className="p-2">
+											<p className="text-xs inline-flex items-center">
+												<AiOutlineMail
+													size={20}
+													className="mr-2 cursor-pointer text-primary hover:text-error"
+												/>
+												{element.value}
+											</p>
+										</li>
+									);
+								case "tel":
+									return (
+										<li key={i} className="p-2">
+											<p className="text-xs inline-flex items-center">
+												<AiFillPhone
+													size={20}
+													className="mr-2 cursor-pointer text-primary hover:text-error"
+												/>{" "}
+												{element.value}
+											</p>
+										</li>
+									);
+								case "facebook":
+									return (
+										<li key={i} className="p-2">
+											<p className="text-xs inline-flex items-center">
+												<AiFillFacebook
+													size={20}
+													className="mr-2 cursor-pointer text-primary hover:text-error"
+												/>
+												{element.value}
+											</p>
+										</li>
+									);
+								case "instagram":
+									return (
+										<li key={i} className="p-2">
+											<p className="text-xs inline-flex items-center">
+												<AiFillInstagram
+													size={20}
+													className="mr-2 cursor-pointer text-primary hover:text-error"
+												/>
+												{element.value}
+											</p>
+										</li>
+									);
+								default:
+									<li key={i} className="p-2">
+										No socials available
+									</li>;
+							}
+						}
+					})}
+				</ul>
+
+				<div className="card-actions justify-end"></div>
+			</div>
+		</div>
+	);
 };
 
 export default RestaurantCard;
