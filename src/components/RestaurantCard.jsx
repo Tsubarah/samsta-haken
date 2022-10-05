9cceedb48d0d412133b85761e147fb18a9171256
Hanna Håkanson <hannahakanson@live.se>
2022-10-05 09:58:03 +0200

    Added address to restaurant card

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -4,7 +4,7 @@ import useUploadImage from "../hooks/useUploadImage";
 import Carousel from "./Carousel";
 import Alert from "./Alert";
 import placeholder from "../assets/images/placeholder-image.webp";
-import { MdAddPhotoAlternate } from "react-icons/md";
+import { MdAddPhotoAlternate, MdOutlinePlace } from "react-icons/md";
 import {
 	AiFillFacebook,
 	AiOutlineGlobal,
@@ -39,174 +39,181 @@ const RestaurantCard = ({ restaurant, currentUser, isAdmin, showDistance }) => {
 	};
 
 	return (
-		<div className="card card-compact rounded-none w-full lg:w-96 bg-base-100 shadow-xl scrollbar-thin scrollbar-thumb-base-content scrollbar-track-black">
-			{restaurant?.photos?.length !== 0 ? (
-				<Carousel restaurant={restaurant} />
-			) : (
-				<img src={placeholder} alt="placeholder" />
-			)}
+    <div className="card card-compact rounded-none w-full lg:w-96 bg-base-100 shadow-xl scrollbar-thin scrollbar-thumb-base-content scrollbar-track-black">
+      {restaurant?.photos?.length !== 0 ? (
+        <Carousel restaurant={restaurant} />
+      ) : (
+        <img src={placeholder} alt="placeholder" />
+      )}
 
-			<div className="card-body">
-				<div className="flex gap-4 items-center">
-					<h2 className="card-title">{restaurant?.name}</h2>
+      <div className="card-body">
+        <div className="flex gap-4 items-center">
+          <h2 className="card-title">{restaurant?.name}</h2>
+          
+          <span className="text-xs opacity-70 font-light">
+            {showDistance(restaurant) &&
+              `${Math.floor(showDistance(restaurant))} km från vald postion`}
+          </span>
 
-					<span className="text-xs opacity-70 font-light">
-						{showDistance(restaurant) &&
-							`${Math.floor(showDistance(restaurant))} km från vald postion`}
-					</span>
+          <Directions restaurant={restaurant} />
+        </div>
 
-					<Directions restaurant={restaurant} />
-				</div>
+        <div className="upload-photo flex items-center justify-between">
+          <label
+            htmlFor="image"
+            className="border border-base-content p-3 rounded-full self-start flex items-center gap-2">
+            <MdAddPhotoAlternate size={25} />
 
-				<div className="upload-photo flex items-center justify-between">
-					<label
-						htmlFor="image"
-						className="border border-base-content p-3 rounded-full self-start flex items-center gap-2"
-					>
-						<MdAddPhotoAlternate size={25} />
+            {image ? (
+              <span className="text-gray-400 text-sm italic">
+                {image.name} {Math.round(image.size / 1024)} kB
+              </span>
+            ) : (
+              <span className="text-gray-400 text-sm italic">
+                Ingen vald bild
+              </span>
+            )}
 
-						{image ? (
-							<span className="text-gray-400 text-sm italic">
-								{image.name} {Math.round(image.size / 1024)} kB
-							</span>
-						) : (
-							<span className="text-gray-400 text-sm italic">
-								Ingen vald bild
-							</span>
-						)}
+            <input
+              type="file"
+              id="image"
+              className="mt-2 hidden"
+              onChange={handleFileChange}
+            />
+          </label>
 
-						<input
-							type="file"
-							id="image"
-							className="mt-2 hidden"
-							onChange={handleFileChange}
-						/>
-					</label>
+          {image && (
+            <button className="btn btn-circle" onClick={handleUpload}>
+              <AiOutlineCloudUpload size={25} />
+            </button>
+          )}
+        </div>
 
-					{image && (
-						<button className="btn btn-circle" onClick={handleUpload}>
-							<AiOutlineCloudUpload size={25} />
-						</button>
-					)}
-				</div>
+        {uploadProgress !== null && (
+          <progress
+            className="progress progress-warning border border-base-content mt-4 w-full"
+            value={uploadProgress}
+            max="100"></progress>
+        )}
 
-				{uploadProgress !== null && (
-					<progress
-						className="progress progress-warning border border-base-content mt-4 w-full"
-						value={uploadProgress}
-						max="100"
-					></progress>
-				)}
+        {isError && <Alert variant={"alert-error"} message={error.message} />}
 
-				{isError && <Alert variant={"alert-error"} message={error.message} />}
+        {isSuccess && isAdmin ? (
+          <Alert
+            variant={"alert-success"}
+            message={"Bilden har laddats upp!"}
+          />
+        ) : isSuccess && !isAdmin ? (
+          <Alert
+            variant={"alert-success"}
+            message={"Bilden kommer att granskas innan den visas!"}
+          />
+        ) : (
+          ""
+        )}
 
-				{isSuccess && isAdmin ? (
-					<Alert
-						variant={"alert-success"}
-						message={"Bilden har laddats upp!"}
-					/>
-				) : isSuccess && !isAdmin ? (
-					<Alert
-						variant={"alert-success"}
-						message={"Bilden kommer att granskas innan den visas!"}
-					/>
-				) : (
-					""
-				)}
+        <div className="divider"></div>
 
-				<div className="divider"></div>
+        <div className="flex">
+          <div className="badge">{restaurant?.type_of_place}</div>
+          <div className="badge">{restaurant?.offers_food}</div>
+          <div className="badge">{restaurant?.cuisine}</div>
+        </div>
 
-				<div className="flex">
-					<div className="badge">{restaurant?.type_of_place}</div>
-					<div className="badge">{restaurant?.offers_food}</div>
-					<div className="badge">{restaurant?.cuisine}</div>
-				</div>
+        <div className="pt-4">
+          <h3 className="font-bold">Recension</h3>
+          <p>{restaurant?.description}</p>
+        </div>
 
-				<div className="pt-4">
-					<h3 className="font-bold">Recension</h3>
-					<p>{restaurant?.description}</p>
-				</div>
+        <div className="divider"></div>
 
-				<div className="divider"></div>
+        <ul>
+          <li key={restaurant.adress} className="p-2">
+            <p className="text-xs inline-flex items-center">
+              <MdOutlinePlace
+                size={20}
+                className="mr-2 cursor-pointer text-primary hover:text-error"
+              />
+              {restaurant.address}
+            </p>
+          </li>
+          {restaurant?.socials?.map((element, i) => {
+            if (element.value != "") {
+              icon = element.title;
+              switch (icon) {
+                case "hemsida":
+                  return (
+                    <li key={i} className="p-2">
+                      <p className="text-xs inline-flex items-center">
+                        <AiOutlineGlobal
+                          size={20}
+                          className="mr-2 cursor-pointer text-primary hover:text-error"
+                        />
+                        {element.value}
+                      </p>
+                    </li>
+                  );
+                case "e-post":
+                  return (
+                    <li key={i} className="p-2">
+                      <p className="text-xs inline-flex items-center">
+                        <AiOutlineMail
+                          size={20}
+                          className="mr-2 cursor-pointer text-primary hover:text-error"
+                        />
+                        {element.value}
+                      </p>
+                    </li>
+                  );
+                case "tel":
+                  return (
+                    <li key={i} className="p-2">
+                      <p className="text-xs inline-flex items-center">
+                        <AiFillPhone
+                          size={20}
+                          className="mr-2 cursor-pointer text-primary hover:text-error"
+                        />
+                        {element.value}
+                      </p>
+                    </li>
+                  );
+                case "facebook":
+                  return (
+                    <li key={i} className="p-2">
+                      <p className="text-xs inline-flex items-center">
+                        <AiFillFacebook
+                          size={20}
+                          className="mr-2 cursor-pointer text-primary hover:text-error"
+                        />
+                        {element.value}
+                      </p>
+                    </li>
+                  );
+                case "instagram":
+                  return (
+                    <li key={i} className="p-2">
+                      <p className="text-xs inline-flex items-center">
+                        <AiFillInstagram
+                          size={20}
+                          className="mr-2 cursor-pointer text-primary hover:text-error"
+                        />
+                        {element.value}
+                      </p>
+                    </li>
+                  );
+                default:
+                  <li key={i} className="p-2">
+                    No socials available
+                  </li>;
+              }
+            }
+          })}
+        </ul>
 
-				<ul>
-					{restaurant?.socials?.map((element, i) => {
-						if (element.value != "") {
-							icon = element.title;
-							switch (icon) {
-								case "hemsida":
-									return (
-										<li key={i} className="p-2">
-											<p className="text-xs inline-flex items-center">
-												<AiOutlineGlobal
-													size={20}
-													className="mr-2 cursor-pointer text-primary hover:text-error"
-												/>
-												{element.value}
-											</p>
-										</li>
-									);
-								case "e-post":
-									return (
-										<li key={i} className="p-2">
-											<p className="text-xs inline-flex items-center">
-												<AiOutlineMail
-													size={20}
-													className="mr-2 cursor-pointer text-primary hover:text-error"
-												/>
-												{element.value}
-											</p>
-										</li>
-									);
-								case "tel":
-									return (
-										<li key={i} className="p-2">
-											<p className="text-xs inline-flex items-center">
-												<AiFillPhone
-													size={20}
-													className="mr-2 cursor-pointer text-primary hover:text-error"
-												/>
-												{element.value}
-											</p>
-										</li>
-									);
-								case "facebook":
-									return (
-										<li key={i} className="p-2">
-											<p className="text-xs inline-flex items-center">
-												<AiFillFacebook
-													size={20}
-													className="mr-2 cursor-pointer text-primary hover:text-error"
-												/>
-												{element.value}
-											</p>
-										</li>
-									);
-								case "instagram":
-									return (
-										<li key={i} className="p-2">
-											<p className="text-xs inline-flex items-center">
-												<AiFillInstagram
-													size={20}
-													className="mr-2 cursor-pointer text-primary hover:text-error"
-												/>
-												{element.value}
-											</p>
-										</li>
-									);
-								default:
-									<li key={i} className="p-2">
-										No socials available
-									</li>;
-							}
-						}
-					})}
-				</ul>
-
-				<div className="card-actions justify-end"></div>
-			</div>
-		</div>
-	);
+        <div className="card-actions justify-end"></div>
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantCard;
