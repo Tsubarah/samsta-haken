f84288c102190d53f489db10278a60eb466302a4
Raymond Lam <raymond.m.lam@gmail.com>
2022-10-06 14:18:46 +0200

    fixed socials submit for restaurant edit card

--- src/components/RestaurantEditCard.jsx
+++ src/components/RestaurantEditCard.jsx
@@ -35,13 +35,19 @@ const RestaurantEditCard = ({ restaurant }) => {
 			name: checkValue(data.name, restaurant.name),
 			address: checkValue(data.address, restaurant.address),
 			city: checkValue(data.city, restaurant.city),
-			position: dataLatLng.results[0].geometry.location,
+			position: dataLatLng?.results[0]?.geometry?.location,
 			description: checkValue(data.description, restaurant.description),
 			cuisine: checkValue(data.cuisine, restaurant.cuisine),
-			type_of_place: checkValue(data.type_of_place, restaurant.type_of_place),
+			type_of_place: checkValue(
+				data.type_of_place,
+				restaurant.type_of_place
+			),
 			offers_food: checkValue(data.offers_food, restaurant.offers_food),
-			photos: [],
-			socials: checkValue(data.socials, restaurant.socials),
+			website: checkValue(data.website, restaurant.website),
+			email: checkValue(data.email, restaurant.email),
+			tel: checkValue(data.tel, restaurant.tel),
+			facebook: checkValue(data.facebook, restaurant.facebook),
+			instagram: checkValue(data.instagram, restaurant.instagram),
 		});
 
 		reset();
@@ -52,7 +58,9 @@ const RestaurantEditCard = ({ restaurant }) => {
 	return (
 		<div className="card bg-base-content shadow-xl h-full overflow-y-auto scrollbar-hide">
 			<div className="card-body flex flex-col items-stretch">
-				<h2 className="card-title text-white self-center">Redigera haket</h2>
+				<h2 className="card-title text-white self-center">
+					Redigera haket
+				</h2>
 				<form
 					onSubmit={handleSubmit(handleEditSubmit)}
 					className="grid grid-cols-7 p-4 bg-base-content rounded-lg w-full h-full overflow-y-auto scrollbar-hide"
@@ -92,7 +100,8 @@ const RestaurantEditCard = ({ restaurant }) => {
 							{...register("description", {
 								minLength: {
 									value: 10,
-									message: "Lite mer får du skriva om haket...",
+									message:
+										"Lite mer får du skriva om haket...",
 								},
 							})}
 							cols="30"
@@ -117,7 +126,11 @@ const RestaurantEditCard = ({ restaurant }) => {
 							{food &&
 								food.cuisine.map((type, i) => (
 									<option
-										selected={type === restaurant.cuisine ? true : false}
+										selected={
+											type === restaurant.cuisine
+												? true
+												: false
+										}
 										key={i}
 										value={type}
 									>
@@ -136,7 +149,11 @@ const RestaurantEditCard = ({ restaurant }) => {
 							{food &&
 								food.type_of_place.map((type, i) => (
 									<option
-										selected={type === restaurant.type_of_place ? true : false}
+										selected={
+											type === restaurant.type_of_place
+												? true
+												: false
+										}
 										key={i}
 										value={type}
 									>
@@ -155,7 +172,11 @@ const RestaurantEditCard = ({ restaurant }) => {
 							{food &&
 								food.offers_food.map((type, i) => (
 									<option
-										selected={type === restaurant.offers_food ? true : false}
+										selected={
+											type === restaurant.offers_food
+												? true
+												: false
+										}
 										key={i}
 										value={type}
 									>
@@ -165,22 +186,59 @@ const RestaurantEditCard = ({ restaurant }) => {
 						</select>
 					</div>
 
-					<div className="col-span-full flex flex-wrap px-4">
-						<div className="basis-full grid gap-3">
-							{restaurant?.socials?.map((item, index) => (
-								<input
-									key={index}
-									placeholder={item.title}
-									defaultValue={`${item.value}`}
-									{...register(`socials.${index}.value`)}
-									className="label-desc input input-bordered input-sm indent-2 bg-primary"
-								/>
-							))}
-						</div>
+					<div className="col-span-full grid py-2 px-4">
+						<input
+							{...register("website")}
+							type="text"
+							defaultValue={restaurant.website}
+							placeholder="hemsida"
+							className="label-desc input input-bordered input-sm indent-2 bg-primary"
+						/>
 					</div>
 
+					<div className="col-span-full grid py-2 px-4">
+						<input
+							{...register("email")}
+							defaultValue={restaurant.website}
+							type="text"
+							placeholder="e-post"
+							className="label-desc input input-bordered input-sm indent-2 bg-primary"
+						/>
+					</div>
+
+					<div className="col-span-full grid py-2 px-4">
+						<input
+							{...register("tel")}
+							defaultValue={restaurant.tel}
+							type="text"
+							placeholder="tel"
+							className="label-desc input input-bordered input-sm indent-2 bg-primary"
+						/>
+					</div>
+
+					<div className="col-span-full grid py-2 px-4">
+						<input
+							{...register("facebook")}
+							defaultValue={restaurant.facebook}
+							type="text"
+							placeholder="facebook"
+							className="label-desc input input-bordered input-sm indent-2 bg-primary"
+						/>
+					</div>
+
+					<div className="col-span-full grid py-2 px-4">
+						<input
+							{...register("instagram")}
+							defaultValue={restaurant.instagram}
+							type="text"
+							placeholder="instagram"
+							className="label-desc input input-bordered input-sm indent-2 bg-primary"
+						/>
+					</div>
 					<div className="col-span-full flex px-4 mt-12">
-						<button className="btn btn-block bg-primary">Uppdatera</button>
+						<button className="btn btn-block bg-primary">
+							Uppdatera
+						</button>
 					</div>
 				</form>
 			</div>
